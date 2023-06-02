43a5af0b3e113eb6cfbe7176c4b72ad428497375
Alessio <alessio@paddle.com>
2023-06-02 15:15:25 +0100

    Prompt input UI

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/chatgpt-prompt', [\App\Http\Controllers\ChatGptPrompt::class, 'index']);
