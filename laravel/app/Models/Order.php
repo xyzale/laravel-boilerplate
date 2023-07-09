63abebbd57dfffb6274bcdcf90cf2fbbc53a6c1f
Alessio Pieruccetti <apieruccetti@gmail.com>
2023-07-09 10:26:48 +0100

    Created models

---
+++ laravel/app/Models/Order.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * @property int $id
+ * @property int $user_id
+ * @property int $product_id
+ */
+class Order extends Model
+{
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
