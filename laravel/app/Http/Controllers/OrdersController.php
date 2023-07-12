eeaa268a6c2588e2ef3b4fb83f9d498c73459c28
Alessio Pieruccetti <apieruccetti@gmail.com>
2023-07-12 19:13:59 +0100

    Custom request

--- laravel/app/Http/Controllers/OrdersController.php
+++ laravel/app/Http/Controllers/OrdersController.php
@@ -2,11 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Events\OrderCreated;
-use App\Models\Order;
+use App\Http\Controllers\Requests\CreateOrderRequest;
 use App\Service\OrderService;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class OrdersController extends Controller
 {
@@ -14,13 +12,8 @@ class OrdersController extends Controller
     {
     }
 
-    public function createOrder(Request $request): JsonResponse
+    public function createOrder(CreateOrderRequest $request): JsonResponse
     {
-        $request->validate([
-            'user_id' => 'required|int',
-            'product_id' => 'required|int',
-        ]);
-
         $order = $this->service->createOrder($request->get('product_id'), $request->get('user_id'));
 
         return response()->json(['order' => $order->load(['product'])->toArray()]);
