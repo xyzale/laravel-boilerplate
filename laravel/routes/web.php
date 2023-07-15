c4970149da63fbc8ee9a1e8bb96f2a5161fa9ebb
Alessio Pieruccetti <apieruccetti@gmail.com>
2023-07-15 09:06:53 +0100

    Clients view

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,7 +21,11 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::get('/clients', function (\Illuminate\Http\Request $request) {
+    return view('clients', ['clients' => $request->user()->clients ?? []]);
+})->middleware(['auth'])->name('dashboard.clients');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
